60d2d64cacfb1cb4bdb9b3fd6050d572f3c86170
Johnny Schmidt <john.schmidt@airbyte.io>
2025-01-21 11:13:18 -0800

    Load CDK Tests: Expand coverage of funky characters

--- airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/S3DataLakeWriteTest.kt
+++ airbyte-integrations/connectors/destination-s3-data-lake/src/test-integration/kotlin/io/airbyte/integrations/destination/s3_data_lake/S3DataLakeWriteTest.kt
@@ -106,6 +106,12 @@ class GlueWriteTest :
     override fun testUnions() {
         super.testUnions()
     }
+
+    @Test
+    @Disabled("https://github.com/airbytehq/airbyte-internal-issues/issues/11439")
+    override fun testFunkyCharacters() {
+        super.testFunkyCharacters()
+    }
 }
 
 class GlueAssumeRoleWriteTest :
@@ -118,7 +124,15 @@ class GlueAssumeRoleWriteTest :
             )
         ),
         S3DataLakeTestUtil.getAWSSystemCredentialsAsMap()
+    ) {
+    @Test
+    @Disabled(
+        "https://github.com/airbytehq/airbyte-internal-issues/issues/11439"
     )
+    override fun testFunkyCharacters() {
+        super.testFunkyCharacters()
+    }
+}
 
 @Disabled(
     "This is currently disabled until we are able to make it run via airbyte-ci. It works as expected locally"
